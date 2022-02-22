bc8aa788431f38218877d1b4bc025ca0c4bb832a
SAMSON Luca <luca.samson@imt-atlantique.net>
2022-02-22 13:49:07 +0100

    Creation de fichier et début avec Bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return flask.render_template("index.html.jinja2")
 
 
 if __name__ == '__main__':
